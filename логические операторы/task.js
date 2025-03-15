25cc707d563dba6818ff6df954411270a3956a89
Алексей <alexseev617@gmail.com>
2025-03-15 19:17:32 +0300

    Update task.js

--- логические операторы/task.js
+++ логические операторы/task.js
@@ -71,3 +71,22 @@ hasJhonCar ||= 'у джона нет машины'
 if (hasJhonCar == false) {
 	;('у джона нет машины')
 }
+
+-------------------------------------------------------
+	let login = prompt('Введите логин', '')
+
+if (!login) {
+	alert('Отменено')
+} else if (login === 'Админ') {
+	let password = prompt('Введите пароль', '')
+
+	if (!password) {
+		alert('Отменено')
+	} else if (password === 'Я главный') {
+		alert('Здравствуйте')
+	} else {
+		alert('Неверный пароль')
+	}
+} else {
+	alert('Я вас не знаю')
+}
